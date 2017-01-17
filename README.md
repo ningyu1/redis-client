4ffc499125e59f41dbd91709a26d967f5500564a
ningyu <ningbe111@163.com>
2017-01-17 17:32:01 +0800

    导入工程

---
+++ README.md
@@ -0,0 +1,2 @@
+# redis-client
+访问redis的java客户端，在jedis基础上包装，从coding方面优化使用方式
---
+++ src/main/java/cn/tsoft/framework/redis/client/CallBack.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017, Jiuye SCM and/or its affiliates. All rights reserved.
+ * FileName: CallBack.java
+ * Author:   ningyu
+ * Date:     2017年1月11日
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package cn.tsoft.framework.redis.client;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 回调
+ * 
+ * @author ningyu
+ * @date 2017年1月11日 上午9:49:34
+ */
+public interface CallBack<T> {
+
+	T invoke(Jedis jedis);
+	
+}
+
+
